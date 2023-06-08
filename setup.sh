c4d9051741a5679e5d7fdb320263ed5817405c27
Eduardo Olivares <28717476+eduardo-olivares1@users.noreply.github.com>
2023-06-08 00:02:22 -0700

    update apt packages

--- setup.sh
+++ setup.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-# Check for python 3
-function check_py3(){
-	command -v python3 >/dev/null 2>&1
-}
-check_py3
-py3_exists=$?
+# Install system wide prefrences and packages
+./system/system.sh
 
-if [$py3_exists != 0]
-then
-	echo "Python 3 not installed..."
-	echo "Installing python3...."
-	sudo apt install python3
-fi
-
-# Install favorite packages using apt
-
-# Transfer files to appropriate locations
+# Install user prefrences and packages
