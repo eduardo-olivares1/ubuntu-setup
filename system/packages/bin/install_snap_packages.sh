b8ba3cb5e8b0ed73112076b79c49c46b8849f181
Eduardo Olivares <28717476+eduardo-olivares1@users.noreply.github.com>
2023-06-19 09:53:52 -0700

    refactor package installation

--- system/packages/bin/install_snap_packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing snap packages..."
-MY_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
-snap_packages="${MY_PATH}/../data/snap.txt"
-while IFS= read -r line
-do
-   sudo snap install $line
-done < "$snap_packages"
--- system/packages/packages.sh
+++ system/packages/packages.sh
@@ -1,5 +1,40 @@
 echo "Installing packages..."
 MY_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
-$MY_PATH/bin/install_apt_packages.sh
-$MY_PATH/bin/install_snap_packages.sh
-$MY_PATH/bin/install_deb_packages.sh
+
+function install_apt_packages() {
+    echo "Installing apt packages..."
+
+    apt_packages="${MY_PATH}/data/apt.txt"
+    while IFS= read -r line; do
+        echo "Installing $line ..."
+        sudo apt install $line -y
+    done <"$apt_packages"
+}
+
+function install_snap_packages() {
+    echo "Installing snap packages..."
+    
+    snap_packages="${MY_PATH}/data/snap.txt"
+    while IFS= read -r line; do
+        sudo snap install $line
+    done <"$snap_packages"
+}
+
+function install_deb_packages() {
+    echo "Installing deb packages from URL..."
+    
+    deb_packages="${MY_PATH}/data/deb.txt"
+    while IFS= read -r line; do
+        #Create temp forlder in /tmp/tmp.UUID
+        TEMP_DEB="$(mktemp)" &&
+            wget -O "$TEMP_DEB" $line &&
+            sudo dpkg -i "$TEMP_DEB"
+        rm -f "$TEMP_DEB"
+    done <"$deb_packages"
+
+    apt --fix-broken install
+}
+
+install_apt_packages
+install_snap_packages
+install_deb_packages
